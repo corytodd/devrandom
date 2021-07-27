e9a6fcf0b4b84d9649745ccb67710e3c78138ae7
Cory Todd <cory@ardonyx.io>
2021-07-27 13:52:45 -0700

    Add project files.

---
+++ app/public.h
@@ -0,0 +1,10 @@
+#define WHILE(a) \
+__pragma(warning(suppress:4127)) while(a)
+
+//
+// Define an Interface Guid so that app can find the device and talk to it.
+//
+// {73335581-D1C5-4E33-B06C-78131AF4BE95}
+DEFINE_GUID(GUID_DEVINTERFACE_DEVRANDOM,
+    0x73335581, 0xd1c5, 0x4e33, 0xb0, 0x6c, 0x78, 0x13, 0x1a, 0xf4, 0xbe, 0x95);
+
---
+++ devrandom/device.h
@@ -0,0 +1,33 @@
+#include "public.h"
+
+//
+// The device context performs the same job as
+// a WDM device extension in the driver frameworks
+//
+typedef struct _DEVICE_CONTEXT
+{
+    ULONG PrivateDeviceData;  // just a placeholder
+
+} DEVICE_CONTEXT, *PDEVICE_CONTEXT;
+
+//
+// This macro will generate an inline function called WdfObjectGet_DEVICE_CONTEXT
+// which will be used to get a pointer to the device context memory
+// in a type safe manner.
+//
+WDF_DECLARE_CONTEXT_TYPE(DEVICE_CONTEXT)
+
+//
+// Function to initialize the device and its callbacks
+//
+NTSTATUS
+DevRandomDeviceCreate(
+    PWDFDEVICE_INIT DeviceInit
+    );
+
+//
+// Device events
+//
+EVT_WDF_DEVICE_SELF_MANAGED_IO_INIT DevRandomEvtDeviceSelfManagedIoStart;
+EVT_WDF_DEVICE_SELF_MANAGED_IO_SUSPEND DevRandomEvtDeviceSelfManagedIoSuspend;
+
